71969a6990b2ed342b5afa4dfa8da30738cfcb37
Adrianacmy <adrianamullaney@gmail.com>
2017-08-09 22:31:06 -0700

    init commit

---
+++ src/main/java/com/example/cheesemvn/CheeseMvnApplication.java
@@ -0,0 +1,12 @@
+package com.example.cheesemvn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheeseMvnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheeseMvnApplication.class, args);
+	}
+}
